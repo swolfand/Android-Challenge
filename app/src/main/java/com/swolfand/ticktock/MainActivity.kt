83330556bd82b5efe100d85f28cdde5e24dc697e
Sam Wolfand <swolfand@slack-corp.com>
2020-07-06 20:39:30 -0700

    add hilt

--- app/src/main/java/com/swolfand/ticktock/MainActivity.kt
+++ app/src/main/java/com/swolfand/ticktock/MainActivity.kt
@@ -7,6 +7,7 @@ import com.swolfand.ticktock.databinding.ActivityMainBinding
 private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
